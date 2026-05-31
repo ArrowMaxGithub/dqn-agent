199067ecccbc34a252e35845b0da39fef5bd5c07
Maximillian Pfeil <pfeil.maximillian@gmail.com>
2026-06-01 00:22:11 +0200

    refactor self play

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import os
+import math
 
 from pathlib import Path
 
 import torch
-from ray.rllib.utils.metrics import ENV_RUNNER_RESULTS, EVALUATION_RESULTS
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
@@ -19,20 +19,20 @@ from random_agent import RandomMaskedRLModule
 def main():
     print(f"GPU supported: {torch.cuda.is_available()}")
 
-    experiment_name = f"opponent_pool_{datetime.now()}"
+    experiment_name = f"self_play_{datetime.now()}"
     params = {
-        "learning_rate": 1e-4,
-        "iterations": 8192,
+        "learning_rate": 2e-4,
+        "iterations": 4096,
         "epsilon_schedule": "linear",
         "epsilon_decay": 0.67,
         "initial_epsilon": 1.0,
-        "final_epsilon": 0.05,
+        "final_epsilon": 0.1,
         "num_env_runners": 16,
         "num_envs_per_env_runner": 8,
-        "replay_buffer_capacity": 65536 * 16,
+        "replay_buffer_capacity": 65536 * 32,
         "double_q": True,
         "train_batch_size": 2048,
-        "num_steps_sampled_before_learning_starts": 65536 * 4,
+        "num_steps_sampled_before_learning_starts": 65536 * 16,
         "target_network_update_freq": 4,
         "td_error_loss_fn": "huber",
         "n_step": 5,
@@ -43,10 +43,12 @@ def main():
         "gamma": 0.99,
         "training_intensity": 1.0,
         "num_eval_env_runners": 16,
-        "eval_episodes": 100,
-        "self_play_capacity": 32,
-        "self_play_interval": 512,
-        "self_play_qualify": 0.1,  # TODO
+        "eval_episodes": 1000,
+        "eval_interval": 8,
+        "self_play_capacity": 64,
+        "self_play_interval": 64,
+        "self_play_recency_bias": 0.25,
+        "self_play_confidence": 0.05,
     }
 
     opponents = {
@@ -57,10 +59,9 @@ def main():
     checkpoint_path = Path(f"./checkpoints/{experiment_name}").resolve()
     os.makedirs(checkpoint_path, exist_ok=True)
 
-    config = dqn_config(
+    algorithm = dqn_config(
         params=params, opponents=opponents, checkpoint_path=checkpoint_path
     )
-    algo = config.build_algo()
     epsilon_decay = EpsilonDecay.from_params(params=params)
 
     log_dir = Path(f"./ray_results/{experiment_name}").resolve()
@@ -72,11 +73,11 @@ def main():
 
     try:
         epsilon = epsilon_decay.get(0)
-        set_epsilon(epsilon=epsilon, algo=algo)
-        warmup(algo=algo, iterations=params["warmup_iterations"])
+        set_epsilon(epsilon=epsilon, algorithm=algorithm)
+        warmup(algorithm=algorithm, iterations=params["warmup_iterations"])
         train(
             w=writer,
-            algo=algo,
+            algorithm=algorithm,
             epsilon_decay=epsilon_decay,
             iterations=params["iterations"],
         )
@@ -90,33 +91,33 @@ def main():
     finally:
         final_path = Path(f"{checkpoint_path}/final").resolve()
         print(f"Saving final version to {final_path}")
-        algo.save(final_path)
-        algo.stop()
+        algorithm.save(final_path)
+        algorithm.stop()
 
 
-def warmup(algo, iterations):
+def warmup(algorithm, iterations):
     pbar = tqdm(range(iterations))
     for i in pbar:
-        results = algo.train()
-
-        eval_runners = results.get(EVALUATION_RESULTS, {}).get(ENV_RUNNER_RESULTS, {})
-        agent_returns = eval_runners.get("agent_episode_returns_mean", {})
-        agent_returns_mean = agent_returns.get("Player 1", 0.0)
-        pbar.set_description(f"Warmup | Reward: {agent_returns_mean:.3f}")
+        algorithm.train()
+        pbar.set_description("Warmup")
 
 
-def train(w, algo, epsilon_decay, iterations):
+def train(w, algorithm, epsilon_decay, iterations):
+    last_eval = None
     pbar = tqdm(range(iterations))
     for i in pbar:
         epsilon = epsilon_decay.get(i)
-        set_epsilon(epsilon=epsilon, algo=algo)
+        set_epsilon(epsilon=epsilon, algorithm=algorithm)
 
-        result = algo.train()
+        result = algorithm.train()
 
-        eval_runners = result.get(EVALUATION_RESULTS, {}).get(ENV_RUNNER_RESULTS, {})
+        eval_runners = result.get("evaluation", {}).get("env_runners", {})
         agent_returns = eval_runners.get("agent_episode_returns_mean", {})
-        agent_returns_mean = agent_returns.get("Player 1", 0.0)
-        w.add_scalar("eval/agent_episode_returns_mean", agent_returns_mean, i)
+        if "Player 1" in agent_returns:
+            return_mean = agent_returns["Player 1"]
+            if not math.isnan(return_mean):
+                w.add_scalar("eval/agent_episode_returns_mean", return_mean, i)
+                last_eval = return_mean
 
         em = result.get("env_runners", {})
         w.add_scalar("env/episode_len_mean", em.get("episode_len_mean", 0), i)
@@ -129,11 +130,17 @@ def train(w, algo, epsilon_decay, iterations):
         w.add_scalar("learner/qf_mean", lm.get("qf_mean", 0), i)
         w.add_scalar("learner/qf_min", lm.get("qf_min", 0), i)
 
-        w.add_scalar("self_play/opponent_pool_size", lm.get("opponent_pool_size", 0), i)
+        if "opponent_pool_size" in result:
+            w.add_scalar(
+                "self_play/opponent_pool_size", result["opponent_pool_size"], i
+            )
 
-        pbar.set_description(
-            f"Training | Epsilon: {epsilon:.3f} Reward: {agent_returns_mean:.3f}"
-        )
+        if last_eval is None:
+            pbar.set_description(f"Training | Epsilon: {epsilon:.3f} Last eval: None")
+        else:
+            pbar.set_description(
+                f"Training | Epsilon: {epsilon:.3f} Last eval: {last_eval:.3f}"
+            )
 
 
 if __name__ == "__main__":
